5e5d99909b2d94a64c8f53bafe73d90ee83568f1
Nathan Y. C <50110608+nathanychin@users.noreply.github.com>
2023-01-15 15:52:58 -0500

    Added routes to App.js

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const logger = require('morgan');
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
+const contactRouter = require('./app_server/routes/contact');
+const mealsRouter = require('./app_server/routes/meals');
+const newsRouter = require('./app_server/routes/news');
+const aboutRouter = require('./app_server/routes/about');
+const roomsRouter = require('./app_server/routes/rooms');
+const usersRouter = require('./app_server/routes/users');
 
 const app = express();
 
@@ -25,9 +31,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/contact', contactRouter);
+app.use('/meals', mealsRouter);
+app.use('/news', newsRouter);
+app.use('/about', aboutRouter);
+app.use('/rooms', roomsRouter);
+app.use('/users', usersRouter);
+
 
 
 // catch 404 and forward to error handler
